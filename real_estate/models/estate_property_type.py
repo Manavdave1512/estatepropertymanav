3e225a2703ef5d542581bfddd62a62e919d6fbbe
Manavdave1512 <manavdave1512@gmail.com>
2025-02-26 14:16:14 +0530

    second commit

--- real_estate/models/estate_property_type.py
+++ real_estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class PropertyType(models.Model):
     _name = "estate_property_type"
@@ -8,11 +8,18 @@ class PropertyType(models.Model):
     name = fields.Char(string='Name',default="unknown",required=True)
     sequence = fields.Integer('Sequence')
     property_ids = fields.One2many('real_estate', 'property_type_id')
-
+    test_ids = fields.One2many("estate_property_offer", "property_id", string="Tests")
+    offer_count = fields.Integer('Offers' ,compute="_compute_countoffer")
+      
     _sql_constraints = [
         ('unique_property_type_name', 'unique(name)', 'The property type name must be unique.')
     ]
 
+    @api.depends('test_ids')
+    def _compute_countoffer(self):
+        for record in self:
+            record.offer_count = len(record.test_ids)
+    
     
 
    
